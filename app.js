ad75cf7934cdd49598e98767b70f84d6270329b5
hoaihvfx14904 <96828188+hoaihvfx14904@users.noreply.github.com>
2022-03-25 19:00:21 +0700

    caidat express

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
-const { fstat } = require('fs');
-const http  = require('http');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Mesage </title></head>');
-        res.write('<body><form action="/message" method="POST><input type="text" name="message"></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if( url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () =>{
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>Hello from nodejs server</h1></body>');
-    res.write('</html>');
-    res.end();
-})
+const http  = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+    
 server.listen(3000);
