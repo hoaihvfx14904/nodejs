f869484b570629e3bb35f16ac8ee19f1b3951450
hoaihvfx14904 <96828188+hoaihvfx14904@users.noreply.github.com>
2022-03-25 12:02:04 +0700

    lab1.4

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http  = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Mesage </title></head>');
+        res.write('<body><form action="/message" method="POST><input type="text" name="message"></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first page</title></head>');
