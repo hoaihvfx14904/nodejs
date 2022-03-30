12e0b0ecb0e99b739c16df050ccf2ae5a748c8b7
hoaihvfx14904 <96828188+hoaihvfx14904@users.noreply.github.com>
2022-03-30 23:34:33 +0700

    lab2.9

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencode({extended: false}));
 
-
 app.use(shopRoutes);
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.use((req, res, next) =>{
     res.status(404).send('<h1> page not found</h1>');
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,13 @@
 const express = require('express');
+
 const router = express.Router();
+
 router.get('/add-product', (req, res, next) =>{
-    res.setHeader().send('<form action="/product" method="POST><input type="text" name="title"><button type="submit"></form>');
+    res.send('<form action="/add/add-product" method="POST><input type="text" name="title"><button type="submit"></form>');
 
 });
-router.post('/product', (req, res, next) => {
+
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 }); 
