1fe8109c7e255c147130b6871991dbf87a4ad8d4
hoaihvfx14904 <96828188+hoaihvfx14904@users.noreply.github.com>
2022-03-30 22:41:25 +0700

    lab2.3

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ app.use((req, res, next) =>{{
 }});
 app.use((req, res, next) =>{
     console.log('in another middleware');
+    res.send('<h1>hello from express</h1>');
+
 })
 
 const server = http.createServer(app);
     
 server.listen(3000);
+
