3cea86bd460d28472d967b99117e1e835ff8ac98
hoaihvfx14904 <96828188+hoaihvfx14904@users.noreply.github.com>
2022-03-30 23:17:07 +0700

    lab2.7

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 
-const http  = require('http');
-const express = require('express');
-
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencode({extended: false}));
-app.use('/add-product', (req, res, next) =>{
-    res.send('<form action="/product" method="POST><input type="text" name="title"><button type="submit"></form>');
 
-});
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/', (req, res, next) => {
-    res.send('<h1> Hello from express</h1>');
-});
 
-const server = http.createServer(app);
+app.use(shopRoutes);
+app.use(adminRoutes);
+
+
+
     
-server.listen(3000);
+app.listen(3000);
 
