6cb34aeb83366be78bd25ce4c16dc63b6fb2770b
hoaihvfx14904 <96828188+hoaihvfx14904@users.noreply.github.com>
2022-03-30 22:47:14 +0700

    lab2.4

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const http  = require('http');
 const express = require('express');
 
 const app = express();
-app.use((req, res, next) =>{{
-    console.log('In the middleware');
+app.use('/', (req, res, next) =>{{
+    console.log('this always runs!');
     next();
 }});
-app.use((req, res, next) =>{
+app.use('/add-product', (req, res, next) =>{
     console.log('in another middleware');
-    res.send('<h1>hello from express</h1>');
+    res.send('<h1>the "add product" page </h1>');
 
-})
+});
+app.use('/', (req, res, next) => {
+    console.log('in another middleware');
+    res.send('<h1> Hello from express</h1>');
+});
 
 const server = http.createServer(app);
     
