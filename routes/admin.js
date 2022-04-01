a8231bbe35b50b95adbf736586160bbe60b4737f
hoaihvfx14904 <96828188+hoaihvfx14904@users.noreply.github.com>
2022-04-01 19:59:48 +0700

    lab2.13

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,17 @@
+const path = require('path');
+
 const express = require('express');
 
+const rootDir = require('../util/path');
 const router = express.Router();
 
+// admin/add-product => get
 router.get('/add-product', (req, res, next) =>{
     res.send('<form action="/add/add-product" method="POST><input type="text" name="title"><button type="submit"></form>');
 
 });
 
+// /admin/add-product => Post
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
